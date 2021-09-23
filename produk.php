33d205179c54906f595afd599731932ba3847a76
david bhisma laksana <90442985+davidbhsm@users.noreply.github.com>
2021-09-23 21:35:44 +0700

    Update produk.php
    Menambahkan produk 2, membuat property dan method

--- produk.php
+++ produk.php
@@ -6,6 +6,7 @@ class Produk {
             $harga = 0;
 
     public function getLabel(){
+        return"$this->penulis, $this->penerbit";
         
     }
 }
@@ -15,3 +16,13 @@ $produk1->judul = "Naruto";
 $produk1->penulis = "Massashi Kishimoto";
 $produk1->penerbit = "Shoonen Jump";
 $produk1->harga = "19.000"; 
+
+$produk2 = new Produk(); // Pertemuan 3 oophp : Membuat propperty dan method
+$produk2->judul = "Dota2";
+$produk2->penulis = "Lord Gaben";
+$produk2->penerbit = "Valve";
+$produk2->harga = "free";
+
+echo"Komik : ". $produk1->getLabel();
+echo <br>
+echo"Game : ". $produk2->getLabel();
